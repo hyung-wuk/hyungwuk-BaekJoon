5783814251b46f5da5276e60ddbd81b4305e60e1
hyung-wuk <2wook040809@gmail.com>
2026-03-04 02:56:23 +0900

    2026.03.03-BaekJoon-commit

---
+++ 2026.03.03-BaekJoon/src/poortypeexample/loop/example2439.java
@@ -0,0 +1,21 @@
+package poortypeexample.loop;
+
+import java.util.Scanner;
+
+public class example2439 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <=N; i++) {
+            for (int j = N-i; j>=1; j--){
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
