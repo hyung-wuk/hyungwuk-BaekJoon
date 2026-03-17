98220dfd7e60fa59d386c2df14d658bc5f2a54a8
hyung-wuk <2wook040809@gmail.com>
2026-03-17 12:02:25 +0900

    2026.03.12-BaekJoon-commit

---
+++ 2026.03.12-BaekJoon/src/array/example2675.java
@@ -0,0 +1,46 @@
+package array;
+
+//import java.util.Scanner;
+//
+//public class example2675 {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int T = sc.nextInt();
+//
+//        for (int i = 1; i <= T; i++) {
+//            int R = sc.nextInt();
+//            sc.nextLine(); //공백 제거
+//            String S = sc.nextLine();
+//            String[] arr = S.split("");
+//
+//            for (int j = 0; j <arr.length; j++) {
+//                for (int k = 0; k < R; k++) {
+//                    System.out.print(arr[j]);
+//                }
+//            }
+//            System.out.println();
+//
+//        }
+//    }
+//}
+
+import java.util.Scanner;
+
+public class example2675 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            int R = sc.nextInt();
+            String S = sc.next();
+
+            for (int j = 0; j < S.length(); j++) {
+                for (int k = 0; k < R; k++) {
+                    System.out.print(S.charAt(j));
+                }
+            }
+            System.out.println();
+        }
+    }
+}
