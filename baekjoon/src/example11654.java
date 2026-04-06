ec9614f5d2e20c1969fbe16fa5f0848df6b3cc7b
hyung-wuk <2wook040809@gmail.com>
2026-04-07 00:51:35 +0900

    2026.04.06-BaekJoon-commit

---
+++ baekjoon/src/example11654.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class example11654 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.nextLine().charAt(0);
+        int num = (int)ch ;
+        System.out.println(num);
+    }
+}
